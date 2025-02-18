4e55fe29934f153e9021a52042099026f95391b2
Alumno <Alumno@B312-PL15.alumnos.uem.es>
2025-02-18 13:12:52 +0100

    ClaseSalónAñadida

---
+++ Casa/src/Estancias/Salón.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package Estancias;
+
+/**
+ * 
+ */
+public class Salón extends Estancia{
+	int nTomasCorriente;
+	boolean tieneTerraza;
+	int superficieTerraza;
+	
+	public void setNTomasCorriente (int nTomasCorriente) {
+		this.nTomasCorriente = nTomasCorriente;
+	}
+	
+	public int getNTomasCorriente() {
+		return nTomasCorriente;
+	}
+	
+	public void setTerraza(String resp) {
+		if (resp.toUpperCase().startsWith("S")) {
+			tieneTerraza = true;
+		} else if (resp.toUpperCase().startsWith("N")){
+			tieneTerraza = false;
+		} else {
+			throw new ArithmeticException("Por favor, de una respuesta del tipo S/N o Si/No");
+		}
+	}
+	
+	public String tieneTerraza () {
+		if (tieneTerraza) {
+			return "Con terraza de " + superficieTerraza + " metros cuadrados.";
+		}else {
+			return "Sin terraza.";
+		}
+	}
+	
+	public void setSuperficieTerraza(int superficieTerraza) {
+		this.superficieTerraza = superficieTerraza;
+	}
+	
+	public String escribirTerraza () {
+		return "Salón "+ this.tieneTerraza();
+	}
+
+}
