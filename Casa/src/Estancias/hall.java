57bf5bf8c92201e265d34bc1c172977cc119358e
Alumno <Alumno@B312-PL15.alumnos.uem.es>
2025-02-18 12:24:53 +0100

    AñadidaClaseHall

--- Casa/src/Estancias/hall.java
+++ Casa/src/Estancias/hall.java
@@ -6,6 +6,28 @@ package Estancias;
 /**
  * 
  */
-public class hall {
-
+public class hall extends Estancia {
+	private boolean puertaBlindada;
+	
+	public void getPuertaBlindada(String resp) {
+		if (resp.toUpperCase().startsWith("S")) {
+			puertaBlindada = true;
+			
+		} else if (resp.toUpperCase().startsWith("N")) {
+			puertaBlindada = false;
+			
+		} else {
+			throw new ArithmeticException("Por favor, de una respuesta del tipo (S/N) o (Sí/No)");
+		}
+	}
+	
+	public String estaLaPuertaBlindada() {
+		if (puertaBlindada) {
+			return " Puerta Blindada";
+		}else {
+			return " Puerta No Blindada";
+		}
+	}
+	
+	
 }
