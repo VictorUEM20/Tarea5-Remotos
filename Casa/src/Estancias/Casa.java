a6783780538de43a88702b660f0b28981d033b86
Alumno <Alumno@B312-PL15.alumnos.uem.es>
2025-02-18 10:47:37 +0100

    root commit

---
+++ Casa/src/Estancias/Casa.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Estancias;
+
+/**
+ * 
+ */
+public class Casa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Casa/src/Estancias/Estancia.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package Estancias;
+
+/**
+ * 
+ */
+public class Estancia {
+	private String nombre;
+	private int metrosCuadrados;
+	private int nPuertas;
+	private int nVentanas;
+	
+	public void estancia(String nombre, int metrosCuadrados, int nPuertas, int nVentanas) {
+		this.nombre = nombre;
+		this.metrosCuadrados = metrosCuadrados;
+		this.nPuertas = nPuertas;
+		this.nVentanas = nVentanas;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+	
+	public void setMetrosCuadrados( int metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+	
+	public int getNPuertas () {
+		return nPuertas;
+	}
+	
+	public void setNPuertas(int nPuertas) {
+		this.nPuertas = nPuertas;
+	}
+	
+	public int getNVentanas () {
+		return nVentanas;
+	}
+	
+	public void setNVentanas (int nVentanas) {
+		this.nVentanas = nVentanas;
+	}
+}
